ad93daf2b1ed3f6a96037973d8d45928c5fea080
= <=>
2021-07-14 22:11:35 -0300

    initialized repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# homeAssistant - camel project
---
+++ src/main/java/br/com/bmo/homeAssistant/App.java
@@ -0,0 +1,13 @@
+package br.com.bmo.homeAssistant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
